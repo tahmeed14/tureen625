de61fc79403cfead03059395fbe37f27df3bce60
Tahmeed Tureen <tureen@umich.edu>
2019-11-21 12:21:49 -0500

    first commit

---
+++ R/hello.R
@@ -0,0 +1,15 @@
+#' @title Says Hello
+#'
+#' @description
+#'
+#' @param person
+#'
+#' @return NULL
+#'
+#' @examples hello("World")
+#'
+#' @export
+
+hello <- function(person) {
+  print(paste("Hello", person))
+}
